86346ecf442681f39935e0c6aee0d0befc42ff4d
Supasan Komonlit <supasan_224@hotmail.com>
2019-10-24 12:22:02 +0700

    Base class for subscriber wait for testing

---
+++ dynamic_src/zeabus/ros/subscriber/base_class.cpp
@@ -0,0 +1,53 @@
+// FILE			: base_class.cpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, October 24 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+namespace zeabus_ros
+{
+
+namespace subscriber
+{
+
+    template< class data_type >
+    BaseClass::BaseClass( ros::NodeHandle* ptr_node_handle , data_type* ptr_data )
+    {
+        this->ptr_node_handle = ptr_node_handle;
+        this->ptr_data = ptr_data;
+    } // function constructor
+
+    template< class data_type >
+    void BaseClass::callback( const data_type& message )
+    {
+        this->ptr_mutex_data->lock();
+        *( this->ptr_data ) = message;
+        this->ptr_mutex_data->unlock();
+    } // function callback
+
+    template< class data_type >
+    void BaseClass::setup_mutex_data( std::mutex* ptr_mutex_data )
+    {   
+        this->ptr_mutex_data = ptr_mutex_data;
+    } // function setup_mutex_data
+
+    template< class data_type >
+    void BaseClass::setup_subscriber( std::string topic , unsigned int queue_size ){
+        this->subscribe = this->ptr_node_handle->subscribe( topic, 
+                queue_size, 
+                &zeabus_ros::subscriber::BaseClass< data_type >::callback,
+                this );
+    } // function setup_subscriber
+
+} // namespace subscriber
+
+} // namespace zeabus_ros
---
+++ include/zeabus/ros/subscriber/base_class.hpp
@@ -0,0 +1,58 @@
+// FILE			: base_class.hpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, October 24 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+#include    <iostream>
+
+#include    <ros/ros.h>
+
+#include    <mutex>
+
+#ifndef _ZEABUS_ROS_SUBSCRIBER_BASE_HPP__
+#define _ZEABUS_ROS_SUBSCRIBER_BASE_HPP__
+
+namespace zeabus_ros
+{
+
+namespace subscriber
+{
+
+    template< class data_type >
+    class BaseClass
+    {
+        public:
+            BaseClass( ros::NodeHandle* ptr_node_handle , data_type* ptr_data );
+
+            void callback( const data_type& message );
+
+            void setup_mutex_data( std::mutex* ptr_mutex_data );
+
+            void setup_subscriber( std::string topic );
+
+        public:
+            data_type* ptr_data;
+
+            std::mutex* ptr_mutex_data;
+
+            ros::NodeHandle* ptr_node_handle;
+
+            ros::Subscriber subscribe_server;
+    }
+
+} // namespace subscriber
+
+} // namespace zeabus_ros
+
+#include    <zeabus/ros/subscriber/base.cpp>
+#endif // _ZEABUS_ROS_SUBSCRIBER_BASE_HPP__

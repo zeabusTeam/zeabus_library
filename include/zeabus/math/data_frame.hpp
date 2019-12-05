fde9e7d50bd2b6894105133c961372fb8d0044e9
SupasanKomonlit <supasan_224@hotmail.com>
2019-12-05 12:19:39 +0700

    Add free space of Data frame

---
+++ include/zeabus/math/data_frame.hpp
@@ -0,0 +1,29 @@
+// FILE			: data_frame.hpp
+// AUTHOR		: K.Supasan
+// CREATE ON	: 2019, December 5 (UTC+0)
+// MAINTAINER	: K.Supasan
+
+// MACRO DETAIL
+
+// README
+
+// REFERENCE
+
+// MACRO SET
+
+// MACRO CONDITION
+
+#ifndef _ZEABUS_MATH_DATA_FRAME_HPP__
+#define _ZEABUS_MATH_DATA_FRAME_HPP__
+
+namespace zeabus
+{
+
+namespace math
+{
+
+} // namespace math
+
+} // namespace zeabus
+
+#endif

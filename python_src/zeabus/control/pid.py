20a28ae5a978fb0e861fab999447482f87d70681
SupasanKomonlit <supasan_224@hotmail.com>
2019-12-22 12:57:05 +0700

    Update pid file constant for saturation

--- python_src/zeabus/control/pid.py
+++ python_src/zeabus/control/pid.py
@@ -12,18 +12,20 @@ from ..math.general import equal
 
 class PID:
 
-    def __init__( self , kp = 0 , ki = 0 , kd = 0 , sample_time = 0.1 , coefficient = 5 ):
+    def __init__( self , kp = 0 , ki = 0 , kd = 0 , 
+            ks = 0 , sample_time = 0.1 , coefficient = 5 ):
         self.input = 0;
         self.output = 0 ;
         self.kp = 0
         self.ki = 0
         self.kd = 0
+        self.ks = 0
         self.offset = 0 
         self.sample_time = sample_time 
         self.i_term = 0
-        self.set_parameter( kp , ki , kd , sample_time , coefficient )
+        self.set_parameter( kp , ki , kd , ks , sample_time , coefficient )
 
-    def set_parameter( self , kp , ki , kd , sample_time , coefficient , offset = 0 ):
+    def set_parameter( self , kp , ki , kd , ks , sample_time , coefficient , offset = 0 ):
         have_reset = False
         if not equal( self.kp , kp ) :
             have_reset = True
@@ -35,12 +37,15 @@ class PID:
             have_reset = True
         elif not equal( self.sample_time , sample_time ):
             have_reset = True
+        elif not equal( self.ks , ks ):
+            have_reset = True
         else:
             None
         self.kp = kp
         self.ki = ki
         self.kd = kd
         self.sample_time = sample_time
+        self.ks = ks
         self.offset = offset
         if have_reset :
             self.reset()
@@ -53,9 +58,8 @@ class PID:
     def calculate( self , input_data , saturation = 0):
         # Part of area , saturation and summation in order
         self.i_term = ( ( self.ki * ( self.input + input_data )/2.0
-                        -saturation * self.ki ) * self.sample_time + 
+                        -saturation * self.ks ) * self.sample_time + 
                         self.i_term )
-        print( self.input , input_data , self.ki ,self.i_term )
         self.d_term = self.kd * ( input_data - self.input ) / self.sample_time
         self.output = self.i_term + self.d_term + self.kp*input_data + self.offset
         self.input = input_data

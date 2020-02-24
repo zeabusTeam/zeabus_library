a000101dcae7418d51ba3c1749100644e53c9678
SupasanKomonlit <supasan_224@hotmail.com>
2020-02-24 11:12:10 +0700

    Add invers inertia

--- include/zeabus/robot.hpp
+++ include/zeabus/robot.hpp
@@ -40,6 +40,8 @@ namespace zeabus
 namespace robot
 {
 
+    namespace bqvm = boost::qvm;
+
     const static double gravity = 9.806;
 
     const static double mass = 35.5;
@@ -61,8 +63,14 @@ namespace robot
         0,      0,      0,      0,      0,      5.4920
     };
 
-    const static boost::qvm::vec< double , 3 > vec_center_gravity = 
-            { -0.0001898 , 0.0060833 , -0.00060831 };
+    const static boost::qvm::mat< double , 6 , 6 > mat_inertia_inverse = {
+        1.0/mass,   0,          0,          0,          0,          0,  
+        0,          1.0/mass,   0,          0,          0,          0,
+        0,          0,          1.0/mass,   0,          0,          0,
+        0,          0,          0,          1.0/6.3663, 0,          0,
+        0,          0,          0,          0,          1.0/5.8800, 0,
+        0,          0,          0,          0,          0,          1.0/5.4920
+    };
 
     const static boost::qvm::mat< double , 3 , 1 > mat_force_gravity = 
             { 0 , 0 , -1.0 * weight};
@@ -73,26 +81,32 @@ namespace robot
     const static boost::qvm::mat< double , 3 , 1 > mat_force_constant =
             { 0 , 0 , force_estimate };
 
-    const static boost::qvm::mat< double , 3 , 3 > mat_center_gravity = 
-            { 0                     , -1.0 * vec_center_gravity.a[2], vec_center_gravity.a[1] ,
-            vec_center_gravity.a[2] , 0                             , -1.0 * vec_center_gravity.a[0],
-            -1.0 * vec_center_gravity.a[1] , vec_center_gravity.a[0], 0 };
+    const static boost::qvm::vec< double , 3 > vec_center_gravity = 
+            { -0.0001898 , 0.0060833 , -0.0060831 };
+
+    // {    0.      -a2,    a1
+    //      a2,     0,      -a0
+    //      -a1,    a0,     0}
+    const static boost::qvm::mat< double , 3 , 3 > mat_center_gravity = {
+            0,              0.0060831,      -0.0060833,
+            -0.0060831,     0,              0.0001898,
+            -0.0060833,     -0.0001898,     0 };
 
     const static boost::qvm::vec< double , 3 > vec_center_buoncy = 
             { 0.0002197 , -0.0070396 , 0.1070393 };
 
-    const static boost::qvm::mat< double , 3 , 3 > mat_center_buoncy = 
-            { 0                     , -1.0 * vec_center_buoncy.a[2] , vec_center_buoncy.a[1] ,
-            vec_center_buoncy.a[2]  , 0                             , -1.0 * vec_center_buoncy.a[0],
-            -1.0 * vec_center_buoncy.a[1] , vec_center_buoncy.a[0]  , 0 };
+    const static boost::qvm::mat< double , 3 , 3 > mat_center_buoncy = {
+            0,              -0.1070393,     -0.0070396,
+            0.1070393,      0,              -0.0002197,
+            0.0070396,      0.0002197,      0}; 
 
     const static boost::qvm::vec< double , 3 > vec_center_constant = 
             { -0.0000144 , 0.0004603 , -0.0004603 };
 
-    const static boost::qvm::mat< double , 3 , 3 > mat_center_constant = 
-            { 0                     , -1.0 * vec_center_constant.a[2] , vec_center_constant.a[1] ,
-            vec_center_constant.a[2] , 0                        , -1.0 * vec_center_constant.a[0],
-            -1.0 * vec_center_constant.a[1] , vec_center_constant.a[0] , 0 };
+    const static boost::qvm::mat< double , 3 , 3 > mat_center_constant = {
+            0,              0.0004603,      0.0004603,
+            -0.0004603,     0,              0.0000144,
+            -0.0004603,     -0.0000144,     0}; 
 
     const static boost::qvm::vec< double , 3 > direction_linear_force[ 8 ] = { 
         { 0 , 0 , 1 },
